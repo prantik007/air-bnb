373b13def937094be6610a9a47089d027e757cf6
PC <prantik007@gmail.com>
2022-02-10 23:24:11 +0530

    added links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,11 @@ function Navbar() {
         <div>
             <nav>
             <img className='logo-image' src={logo} alt="logo" />
+            <ul className='nav-ul-links'>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact US</li>
+            </ul>
 
             </nav>
         </div>
